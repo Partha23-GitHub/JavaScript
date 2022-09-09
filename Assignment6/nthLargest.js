2652fc1d59a3b7113490874cde510206ce34bc14
partha <parthamanna.bit23@gmail.com>
2022-09-09 16:35:05 +0530

    nth

---
+++ Assignment6/nthLargest.js
@@ -0,0 +1,7 @@
+let returnNthNumber = (array, position) => {
+  let newArray = array.sort((a, b) => b - a);
+  return newArray[position - 1];
+};
+
+let array = [3, 45, 6, 7, 23, 5, 7, 8];
+console.log(returnNthNumber(array, 3));
